545a46f564946b2d34233a2ebf4a03106c3b5afb
wdhazleh <wdhazleh@ncsu.edu>
2022-10-30 23:54:26 -0400

    [Implementation] Began implementation of Log class Began implementation of the methods in the Log class, though this is mostly just initialized variables, such as the array of the list, initial capacity, and the size integer.

---
+++ Project2/src/edu/ncsu/csc216/stp/model/util/Log.java
@@ -0,0 +1,44 @@
+package edu.ncsu.csc216.stp.model.util;
+
+/**
+ *  Class for Log objects
+ * @author William Hazlehurst, Erik Svanes
+ *
+ * @param <E> The generic Element that can be added to a log object
+ */
+public class Log<E> implements ILog {
+	/** Static integer which matches the initial capacity of all lists */
+	private static final int INIT_CAPACITY = 10;
+	/** The array for each log object */
+	private E[] log;
+	/** The size of the log object */
+	private int size;
+	
+	/**
+	 * Constructor for a new Log object
+	 */
+	public Log() {
+		log = new Log[INIT_CAPACITY];
+	}
+
+	@Override
+	public void add(Object element) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object get(int idx) {
+		if(idx < 0 || idx > size) {
+			throw new IndexOutOfBoundsException();
+		}
+		return log[idx];
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
