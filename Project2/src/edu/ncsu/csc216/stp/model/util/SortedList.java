305166b71d24c216a5d372c959c2f1e141cadeee
epsvanes <epsvanes@ncsu.edu>
2022-11-02 21:54:47 -0400

    Possibly completed sortedlist add method

--- Project2/src/edu/ncsu/csc216/stp/model/util/SortedList.java
+++ Project2/src/edu/ncsu/csc216/stp/model/util/SortedList.java
@@ -1,27 +1,104 @@
 package edu.ncsu.csc216.stp.model.util;
 
+import edu.ncsu.csc216.stp.model.tests.TestCase;
+
 /**
- * Class for the SortedList object, which is an array of objects sorted by a certain parameter
+ * Class for the SortedList object, which is an array of objects sorted by a
+ * certain parameter
+ * 
  * @author William Hazlehurst, Erik Svanes
  *
  * @param <E> Generic element which can be stored in this given List
  */
 public class SortedList<E> implements ISortedList<E> {
+	/** The first list node in the list */
+	private ListNode front;
 	/** Size of the list as an integer */
 	private int size;
 
+	/**
+	 * The constructor for the sorted list class. This sets the front variable to
+	 * null and the size to zero.
+	 */
+	public SortedList() {
+		front = null;
+		size = 0;
+	}
+
 	@Override
 	public void add(E element) {
-		if(element == null) {
+		// null check the element
+		if (element == null) {
 			throw new NullPointerException("Cannot add null element");
 		}
-		for(int i = 0; i < this.size(); i++) {
-			if(this.get(i) == element) {
-				throw new IllegalArgumentException("Cannot add duplicate element");
+		// check for duplicate elements
+		ListNode current = front;
+		while (current != null) {
+			if (element.equals(current.data)) {
+				throw new IllegalArgumentException("No duplicate elements.");
 			}
+			current = current.next;
 		}
-		// TODO fill in
-		
+		// if first node in the list
+		if (front == null) {
+			front = new ListNode(element);
+			size++;
+			return;
+		}
+		// add the element
+		TestCase tsAdd;
+		TestCase temp;
+		boolean onlyFailed = true;
+		try {
+			tsAdd = (TestCase) element;			
+		} catch (Exception e) {
+			System.out.println("Element is not a test case");
+			return;
+		}
+		current = front;
+		int firstPassingTS = -1;
+		while (current != null) {
+			try {
+				temp = (TestCase) current.next.data;
+			} catch (Exception e) {
+				System.out.println("Could not cast");
+				return;
+			}
+			if (!temp.isTestCasePassing()) {
+				firstPassingTS++;
+			}
+			else if (temp.isTestCasePassing()) {
+				onlyFailed = false;
+			}
+			else {
+				break;
+			}
+			current = current.next;
+		}
+		current = front;
+		// if the list only is passing test cases
+		if (firstPassingTS == -1) {
+			current = new ListNode(element);
+			size++;
+			return;
+		}
+		// if the list only is failed test cases
+		if (onlyFailed) {
+			while (current != null) {
+				current = current.next;
+			}
+			current = new ListNode(element);
+			size++;
+			return;
+		}
+		// if its not only failed test cases
+		for (int i = 0; i < firstPassingTS + 1; i++) {
+			current = current.next;
+		}
+		ListNode savedReference = current.next;
+		current.next = new ListNode(element);
+		current.next.next = savedReference;
+		size++;
 	}
 
 	@Override
@@ -44,8 +121,24 @@ public class SortedList<E> implements ISortedList<E> {
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
+	}
+
+	private class ListNode {
+		/** The data variable for each node */
+		private E data;
+		/** The next list node for each node */
+		private ListNode next;
+
+		public ListNode(E data) {
+			this.data = data;
+			this.next = null;
+		}
+
+		public ListNode(E data, ListNode next) {
+			this.data = data;
+			this.next = next;
+		}
 	}
 
 }
