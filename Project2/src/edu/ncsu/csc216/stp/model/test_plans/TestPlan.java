c9130759d8c6db10114e7d64c71fbe6912ca7532
epsvanes <epsvanes@ncsu.edu>
2022-11-02 20:30:21 -0400

    Added the string parameter in the test plan constructor

--- Project2/src/edu/ncsu/csc216/stp/model/test_plans/TestPlan.java
+++ Project2/src/edu/ncsu/csc216/stp/model/test_plans/TestPlan.java
@@ -13,10 +13,15 @@ import edu.ncsu.csc216.stp.model.tests.TestCase;
  */
 public class TestPlan extends AbstractTestPlan implements Comparable<TestPlan> {
 
+	/** The name of the test plan */
+	private String testPlanName;
+	
 	/**
 	 * Constructor for a TestPlan object
+	 * 
+	 * @param testPlanName name of the Test Plan
 	 */
-	public TestPlan() {
+	public TestPlan(String testPlanName) {
 		// TODO fill in
 	}
 
