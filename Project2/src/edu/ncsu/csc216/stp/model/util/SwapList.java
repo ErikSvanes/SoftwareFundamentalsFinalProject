279dba349a2210344162a4b18757c4c985794a35
epsvanes <epsvanes@ncsu.edu>
2022-11-03 22:49:25 -0400

    implemented multiple methods in swaplist

--- Project2/src/edu/ncsu/csc216/stp/model/util/SwapList.java
+++ Project2/src/edu/ncsu/csc216/stp/model/util/SwapList.java
@@ -2,58 +2,141 @@ package edu.ncsu.csc216.stp.model.util;
 
 /**
  * Class for the SwapList object, which is used to swap elements in a List
+ * 
  * @author William Hazlehurst, Erik Svanes
  *
  * @param <E> Generic elements which can be stored in this type of List
  */
 public class SwapList<E> implements ISwapList {
+	/** Initial size of the array */
+	private static final int INIT_SIZE = 10;
+	/** The array */
+	@SuppressWarnings("unchecked")
+	private E[] list = (E[]) (new Object[INIT_SIZE]);
+	/** The size of the array */
+	private int size = 0;
 
+	/**
+	 * Method that adds an element to the back of the list
+	 * 
+	 * @param element the element to add to the back of the list
+	 * @throws NullPointerException if the element parameter is null
+	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public void add(Object element) {
-		// TODO Auto-generated method stub
-		
+		if (element == null)
+			throw new NullPointerException("Cannot add null element.");
+		if (size == list.length)
+			growArray();
+		list[size - 1] = (E) element;
+		size++;
 	}
 
+	/**
+	 * Method that removes the element at the specified valid index
+	 * 
+	 * @param idx the index of the element to remove
+	 * @throws IndexOutOfBoundsException if the index is out of bounds for the list
+	 */
 	@Override
-	public Object remove(int idx) {
-		// TODO Auto-generated method stub
-		return null;
+	public E remove(int idx) {
+		if (idx < 0 || idx >= size)
+			throw new IndexOutOfBoundsException("Invalid index.");
+		E removedElement = list[idx];
+		for (int i = idx; i < size; i++) {
+			list[i] = list[i + 1];
+		}
+		list[size - 1] = null;
+		size--;
+		return removedElement;
 	}
 
+	/**
+	 * Method that swaps the element at the given index with the element before it
+	 * in the list (index - 1).
+	 * 
+	 * @param idx the index of the element to swap
+	 */
 	@Override
 	public void moveUp(int idx) {
-		// TODO Auto-generated method stub
-		
+		if (idx < 0 || idx >= size)
+			throw new IndexOutOfBoundsException("Invalid index.");
+		swapElements(idx, idx - 1);
 	}
 
+	/**
+	 * Method that swaps the element at the given index with the element after it in
+	 * the list (index - +).
+	 * 
+	 * @param idx the index of the element to swap
+	 */
 	@Override
 	public void moveDown(int idx) {
-		// TODO Auto-generated method stub
-		
+		if (idx < 0 || idx >= size)
+			throw new IndexOutOfBoundsException("Invalid index.");
+		swapElements(idx, idx + 1);
 	}
 
 	@Override
 	public void moveToFront(int idx) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void moveToBack(int idx) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	/**
+	 * Method that returns the element at the specified valid index.
+	 * 
+	 * @param idx the specified valid index
+	 * @return the element of the list at the specified index
+	 */
 	@Override
-	public Object get(int idx) {
-		// TODO Auto-generated method stub
-		return null;
+	public E get(int idx) {
+		if (idx < 0 || idx >= size)
+			throw new IndexOutOfBoundsException("Invalid index.");
+		return list[idx];
 	}
 
+	/**
+	 * Method that returns the size of the list
+	 * 
+	 * @return the size of the array
+	 */
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
+	}
+
+	/**
+	 * Method that doubles the length of the list variable, and is called when
+	 * something is added when the size is equal to the length.
+	 */
+	@SuppressWarnings("unchecked")
+	private void growArray() {
+		int currentLength = list.length;
+		E[] newArr = (E[]) (new Object[currentLength * 2]);
+		for (int i = 0; i < currentLength; i++) {
+			newArr[i] = list[i];
+		}
+		list = newArr;
+	}
+
+	/**
+	 * Swaps two elements in the list given their indexes.
+	 * 
+	 * @param firstIdx  the index of the first element to swap
+	 * @param secondIdx the index of the second element to swap
+	 */
+	private void swapElements(int firstIdx, int secondIdx) {
+		E firstElement = list[firstIdx];
+		list[firstIdx] = list[secondIdx];
+		list[secondIdx] = firstElement;
 	}
 
 }
